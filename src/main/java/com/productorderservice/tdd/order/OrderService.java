bb762a7f3ee1c8b00311b2fe81899bf2c99b7b4b
taegyun1995 <needle12s@gmail.com>
2024-05-13 00:06:03 +0900

    refactor: switching to product order service api controller test code

--- src/main/java/com/productorderservice/tdd/order/OrderService.java
+++ src/main/java/com/productorderservice/tdd/order/OrderService.java
@@ -1,9 +1,17 @@
 package com.productorderservice.tdd.order;
 
 import com.productorderservice.tdd.product.Product;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Component
+@RestController
+@RequestMapping("/orders")
 class OrderService {
 
     private final OrderPort orderPort;
@@ -12,12 +20,16 @@ class OrderService {
         this.orderPort = orderPort;
     }
 
-    public void createOrder(final CreateOrderRequest request) {
+    @PostMapping
+    public ResponseEntity<Void> createOrder(
+            @RequestBody final CreateOrderRequest request
+    ) {
         final Product product = orderPort.getProductById(request.productId());
 
         final Order order = new Order(product, request.quantity());
 
         orderPort.save(order);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
 }
